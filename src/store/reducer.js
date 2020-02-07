c5b54446ecb2d6caedfc680691f5002e0bdfffc9
akinsola <thomas.lawanson@gmail.com>
2020-02-07 14:20:00 +0000

    update

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -51,5 +51,5 @@ const reducer = (state = initalState, action) => {
 export const store = () =>
   createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
