e880c9a6cbf64971f6c92e93d5ebbaebdccdc316
akinsola <thomas.lawanson@gmail.com>
2020-02-07 14:18:00 +0000

    updated site

--- src/components/jumbotron/jumbotron.js
+++ src/components/jumbotron/jumbotron.js
@@ -1,63 +1,91 @@
 import React from "react"
 import styled from "styled-components"
 import { connect } from "react-redux"
-import Img from 'gatsby-image';
+import Img from "gatsby-image"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { richTextOptions } from "../../utility/richtext"
+import * as ActionTypes from "../../store/actions"
+import CloseIcon from "../../images/close_overlay.svg"
 const JumbotronWrapper = styled.section`
-    /* padding: 1rem; */
-    height: 70vh;
+  /* padding: 1rem; */
+  height: 70vh;
 `
 
 const JumbotronModal = styled.div`
-    position: relative;
-    width: 100%;
-    height: 100%;
-    background: yellow;
-    z-index: 1000;
-    display: ${props => props.show ? 'inherit': 'none'}
+  position: relative;
+  width: 100%;
+  height: 100%;
+  /* background: yellow; */
+  z-index: 1000;
+  padding: 1rem;
+  display: ${props => (props.show ? "inherit" : "none")};
 `
+
+const ModalHeader = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+`
+
+const ModalBody = styled.div``
 const JumbotronUnderlay = styled.div`
-    display: ${props => props.show ? 'inherit': 'none'};
-    background: pink;
-    width: auto;
-    height: 100%;
+  display: ${props => (props.show ? "inherit" : "none")};
+  background: pink;
+  width: auto;
+  height: 100%;
 `
 
 const ImageContainer = styled.div`
-    width: 100%;
+  width: 2%;
 `
-const Jumbotron = (props) => {
-    let content = props.jumbotronContent;
-    let image = content.find((im) => {
-        return im.type == "Image";
-    })
+const Jumbotron = props => {
+  let content = props.jumbotronContent
+  let image = content.find(im => {
+    return im.type === "Image"
+  })
 
-    let bio = content.find((bi) => {
-        return bi.type == "Bio";
-    })
+  let bio = content.find(bi => {
+    return bi.type === "Bio"
+  })
 
-    console.log('bio', bio);
-    console.log('image', image);
+  console.log("bio", bio)
+  console.log("image", image)
 
-    return (
-        <JumbotronWrapper>
-            <JumbotronModal show={props.show_modal}>
-                <p> Some text</p>
-            </JumbotronModal>
-            <JumbotronUnderlay show={!props.show_modal}>
-            <ImageContainer>
-            <Img fluid={image.image.fluid} />
+  return (
+    <JumbotronWrapper>
+      <JumbotronModal show={props.show_modal}>
+        <ModalHeader>
+          <ImageContainer>
+            <img src={CloseIcon} onClick={() => props.toggleModal()} />
+          </ImageContainer>
+        </ModalHeader>
 
-            </ImageContainer>
-            </JumbotronUnderlay>
-        </JumbotronWrapper>
-    )
+        <ModalBody>
+          {documentToReactComponents(bio.text.json, richTextOptions)}
+        </ModalBody>
+      </JumbotronModal>
+      <JumbotronUnderlay show={!props.show_modal}>
+        <Img fluid={image.image.fluid} />
+      </JumbotronUnderlay>
+    </JumbotronWrapper>
+  )
 }
 
 const mapStateToProps = state => {
-    return {
-      isLoaded: state.isLoaded,
-      jumbotronContent: state.jumbotron_content,
-      show_modal: state.show_modal
-    }
+  return {
+    isLoaded: state.isLoaded,
+    jumbotronContent: state.jumbotron_content,
+    show_modal: state.show_modal,
   }
-  export default connect(mapStateToProps, null)(Jumbotron)
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    toggleModal: () =>
+      dispatch({
+        type: ActionTypes.TOGGLE_MODAL,
+      }),
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Jumbotron)
