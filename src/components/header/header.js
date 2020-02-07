349c419e5aa04ac7726ce2779257f961991189d3
akinsola <thomas.lawanson@gmail.com>
2020-02-07 09:32:17 +0000

    updated header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,16 +1,47 @@
 import React from "react"
 import styled from "styled-components"
+import { connect } from "react-redux"
 
 const HeaderWrapper = styled.header`
-    
+  padding: 0.5rem;
+  padding-top: 1rem;
 `
 
-const Header = () => {
-    return (
-        <HeaderWrapper>
-            <h1> Jelena Viskovic</h1>
-        </HeaderWrapper>
-    )
-}
+const HeaderTitleContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  /* align-content: space-around; */
+  justify-content: space-between;
+`
 
-export default Header;
+const HeaderLink = styled.a`
+    font-size: 1.4rem;
+`
+
+const HeaderTitle = styled.p`
+    font-size: 1.4rem;
+`
+const Header = (props) => {
+    let links = props.headerLinks;
+    links = links.sort((a, b) => {
+        return a.order - b.order;
+    });
+
+    console.log('PRO', links);
+  return (
+    <HeaderWrapper>
+      <HeaderTitleContainer>
+      {links.map((link, index) => (
+         link.externalLink ? <HeaderLink href={link.url} target="__blank" key={index}> {link.title} </HeaderLink> : <HeaderTitle key={index}> {link.title} </HeaderTitle> 
+      ))}
+      </HeaderTitleContainer>
+    </HeaderWrapper>
+  )
+}
+const mapStateToProps = state => {
+  return {
+    isLoaded: state.isLoaded,
+    headerLinks: state.header_links
+  }
+}
+export default connect(mapStateToProps, null)(Header)
